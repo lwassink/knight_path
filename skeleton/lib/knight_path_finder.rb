4c1139a6b950fa2b755d5841068b5f4e8619b402
App Academy Student <student@appacademy.io>
2016-09-16 11:51:03 -0700

    change nodes to positions, add KnightPathFinder#new_moves

--- skeleton/lib/knight_path_finder.rb
+++ skeleton/lib/knight_path_finder.rb
@@ -4,27 +4,33 @@ class KnightPathFinder
   attr_reader :starting_position
 
   def initialize(starting_position)
-    @starting_position = PolyTreeNode.new(starting_position)
+    @starting_position = starting_position
     @visited_positions = [@starting_position]
   end
 
-  def self.valid_moves(node)
-    x,  y = node.value
+  def self.valid_moves(pos)
+    x,  y = pos
     translations = [[2,1], [2,-1], [1, 2], [1, -2], [-1, 2], [-1, -2], [-2, 1], [-2, -1]]
 
     moves = translations.map do |translation|
       x_trans, y_trans = translation
-      PolyTreeNode.new([x + x_trans, y + y_trans])
+      [x + x_trans, y + y_trans]
     end
 
     moves.select do |move|
-      x, y = move.value
+      x, y = move
       x.between?(0, 7) & y.between?(0, 7)
     end
   end
 
+  def new_moves(pos)
+    new_moves = self.class.valid_moves(pos) - @visited_positions
+    @visited_positions += new_moves
+    new_moves
+  end
+
   def inspect
-    p @starting_position.value
+    puts @starting_position
   end
 
 end
